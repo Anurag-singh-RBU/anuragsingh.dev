3db90b7f71b0fb83230a3338fc5a25827f990489
Anurag-singh-RBU <anuragsingh260805@gmail.com>
2025-09-05 00:54:49 +0530

    timeline

--- components/Contact.js
+++ components/Contact.js
@@ -79,7 +79,7 @@ export default function Contact() {
           type="email"
           autoComplete="email"
           required
-          className="px-4 py-2 placeholder:text-sm mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
+          className="px-4 py-2 placeholder:text-sm placeholder:font-mono mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-gray-300 rounded-md bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100"
         />
         <button
           className="flex items-center justify-center absolute right-1 top-1 px-4 font-bold h-8 bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded w-28"
